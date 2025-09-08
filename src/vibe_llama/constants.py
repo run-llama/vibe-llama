6920bafaa74b0d04976c7f913f6a4121ebc500b2
Clelia (Astra) Bertelli <133636879+AstraBert@users.noreply.github.com>
2025-09-08 15:27:11 +0200

    fix: correct instructions base url (#17)
    * fix: correct URL for instructions
    
    * chore: vbump

--- src/vibe_llama/constants.py
+++ src/vibe_llama/constants.py
@@ -1,2 +1,2 @@
 CHUNKS_SEPARATOR = "<!-- sep---sep -->"
-BASE_URL = "https://raw.githubusercontent.com/run-llama/vibe-llama/clelia/workflows-scaffolding"
+BASE_URL = "https://raw.githubusercontent.com/run-llama/vibe-llama/main"
